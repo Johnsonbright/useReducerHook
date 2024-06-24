8cacb43a1f9cdad859d5303eaa6d88b1129ed2b8
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-06-24 09:05:15 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
+import CounterThree from './components/CounterThree'
+
+function App() {
+return <div className='App'>
+  < CounterThree />
+       {/* <CounterOne /> */}
+       {/* <CounterTwo /> */}
+</div>
+}
+
+export default App
